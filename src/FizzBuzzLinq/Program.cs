4800cb5fd3b0887a0200aa85c5832f7a3092d532
jmeridth <jmeridth@gmail.com>
2012-03-19 09:53:19 -0500

    initial commit

---
+++ src/FizzBuzzLinq/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace FizzBuzzLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Join("\n", Enumerable.Range(1, 100)
+                .Select(i => ((i % 3 == 0) && (i % 5 == 0)) ? "FizzBuzz" 
+                    : (i % 3 == 0) ? "Fizz" 
+                    : (i % 5 == 0) ? "Buzz" 
+                    : i.ToString(CultureInfo.InvariantCulture)).ToArray()));
+
+            Console.ReadLine();
+        }
+    }
+}
